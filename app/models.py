38e7983b51ceb1b40f59440e5aa21b512c72ff37
discountthree <1774631646@qq.com>
2019-01-10 21:48:19 +0800

    完成商品展示

--- app/models.py
+++ app/models.py
@@ -6,6 +6,16 @@ class Banner(models.Model):
     img = models.CharField(max_length=100)
 
 
+# 天天惊喜
+class DailySurprise(models.Model):
+    img = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    price = models.CharField(max_length=20)
+    preprice = models.CharField(max_length=20)
+    activity = models.CharField(max_length=20)
+    span = models.CharField(max_length=20)
+
+
 # 精品推荐
 class Recommendation(models.Model):
     img = models.CharField(max_length=100)
@@ -13,3 +23,10 @@ class Recommendation(models.Model):
     price = models.CharField(max_length=20)
     reason = models.CharField(max_length=20)
     p = models.CharField(max_length=100)
+
+
+# 商品展示
+class Goods(models.Model):
+    img = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    price = models.CharField(max_length=20)
--- app/views.py
+++ app/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 
-from app.models import Banner, Recommendation
+from app.models import Banner, Recommendation, Goods, DailySurprise
 
 
 def homepage(request):
+    # 轮播数据
     banners = Banner.objects.all()
+    # E仔推荐精品
     recommendations = Recommendation.objects.all()
+    # 天天惊喜
+    dailysurprise = DailySurprise.objects.all()
+    # 商品展示
+    goods_list = {}
+    for i in range(0, 6):
+        for j in range(0, 8):
+            goods_list['goods' + str(i)] = Goods.objects.all()[i * 8:(i + 1) * 8]
+    # print(goods_list)
+
     data = {
         'banners': banners,
         'recommendations': recommendations,
+        'dailysurprise': dailysurprise,
+        'goods_list': goods_list,
     }
 
     return render(request, 'homepage.html', context={'data': data})
